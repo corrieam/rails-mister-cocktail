b3ce6056e441c8c732532f39e7c13a3ab0f94727
Corrie Mosley <corrie.mosley@gmail.com>
2019-05-15 11:05:35 +0100

    views added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+lime = Ingredient.create(name: "lime")
+ice = Ingredient.create(name: "ice")
+mint_leaves = Ingredient.create(name: "mint leaves")
+tequila = Ingredient.create(name: "tequila")
+cointreau = Ingredient.create(name: "cointreau")
+lime_juice = Ingredient.create(name: "lime juice")
+white_rum = Ingredient.create(name: "white rum")
+soda = Ingredient.create(name: "soda water")
+
+
+mojito = Cocktail.create(name: "mojito")
+margarita = Cocktail.create(name: "margarita")
+
+Dose.create(description: "1 part", ingredient: tequila, cocktail: margarita)
+Dose.create(description: "0.5 part", ingredient: lime_juice, cocktail: margarita)
+Dose.create(description: "0.5 part", ingredient: cointreau, cocktail: margarita)
+Dose.create(description: "cubed", ingredient: ice, cocktail: margarita)
+
+Dose.create(description: "12 fresh", ingredient: mint_leaves, cocktail: mojito)
+Dose.create(description: "2 parts", ingredient: white_rum, cocktail: mojito)
+Dose.create(description: "a dash of", ingredient: soda, cocktail: mojito)
+Dose.create(description: "crushed", ingredient: ice, cocktail: mojito)
